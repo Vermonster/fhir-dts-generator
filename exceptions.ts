1dbab559467680c95984d761a16233f951404c2e
Brian Kaney <brian@vermonster.com>
2021-04-22 17:01:40 -0400

    WIP

---
+++ exceptions.ts
@@ -0,0 +1,10 @@
+// These are regexes of examples to skip, because the are invalid in the spec
+// :(
+export default {
+  'r2': [
+    'patient-example.canonical.json', // random NULL
+    'patient-example.json', // random NULL
+    'relatedperson-example.canonical.json', // random NULL
+    'relatedperson-example.json' // random NULL
+  ],
+}
--- gulpfile.ts
+++ gulpfile.ts
@@ -3,7 +3,7 @@ import del from 'del'
 import fs from 'fs'
 import wget from 'wget-improved'
 import { exec } from 'child_process'
-
+import exceptions from './exceptions'
 
 // Utils...
 const removeKeys = (obj: object, keys: string[]) : {} => obj !== Object(obj)
@@ -17,26 +17,54 @@ const removeKeys = (obj: object, keys: string[]) : {} => obj !== Object(obj)
     {}
   )
 
-const unzip = (out: string, cb: Function) => {
-  const outDir = out.replace(/(.*)\.zip$/, '$1')
-  console.log(`* Extracting ${out} to ${outDir}...`)
-  exec(`unzip -o ${out} -d ${outDir}`, (error, stdout, stderr) => {
+const cleanEmpty = function (object: {}) {
+  for (let [k, v] of Object.entries(object)) {
+    if (v && typeof v === 'object')
+      cleanEmpty(v)
+
+    if (v &&
+      typeof v === 'object' &&
+      !Object.keys(v).length ||
+      v === null ||
+      v === undefined
+    ) {
+      if (k.startsWith('_')) {
+        if (Array.isArray(object))
+          object.splice(Number(k), 1)
+        else if (!(v instanceof Date))
+          delete (object as any)[k]
+      }
+    }
+  };
+  return object;
+}
+const execute = (command: string, cb: Function) => {
+  exec(command, { maxBuffer: 1024 * 5000 }, (error, _stdout, stderr) => {
     if (error) {
-      console.log(`error: ${error.message}`)
-      return
+      console.error(`warn: ${error.message}`)
     }
     if (stderr) {
-      console.log(`stderr: ${stderr}`)
-      return
+      console.error(`warn: ${stderr}`)
     }
-    console.log(`stdout: ${stdout}`)
     cb()
   })
 }
 
-const download = (url: string, output: string, cb: Function) => {
-  console.log(`* Downloading ${url} to ${output}...`)
-  /*  
+
+const unzip = (out: string, cb: Function) => {
+  const outDir = out.replace(/(.*)\.zip$/, '$1')
+  console.log(`* Extracting ${out} to ${outDir}...`)
+  execute(`unzip -o ${out} -d ${outDir}`, cb)
+}
+
+const doNotSkip = (file: string, specDir: string): boolean => {
+  const version = specDir.slice(-7, -5)
+  return ! (exceptions as any)[version]?.includes(file)
+}
+
+const download = (url: string, dest: string, cb: Function) => {
+  console.log(`* Downloading ${url} to ${dest}...`)
+  /*
   wget.download(url, output)
     .on('error', (err) => console.log(err))
     .on('end', (out) => {
@@ -44,35 +72,44 @@ const download = (url: string, output: string, cb: Function) => {
       cb()
     })
   */
-  unzip(output, cb)
+  // unzip(dest, cb)
+  cb()
 }
 
-export const buildExamples = (examplesDir: string, outputDir: string, cb: Function) => {
-  console.log(`* Reading from ${examplesDir}...`)
-  fs.readdir(examplesDir, (err, files) => {
-    if (err) { 
+export const buildExamples = (specDir: string, outputDir: string, cb: Function) => {
+  console.log(`* Reading from ${specDir}...`)
+  fs.readdir(specDir, (err, files) => {
+    if (err) {
       console.error(err)
-      throw(err) 
+      throw(err)
     }
     const testFile = [
       "import * as fhir from './index';",
-      "let dut;"
     ] as string[]
-    files?.forEach((file) => {
+    files?.forEach((file, i) => {
       if (
-        !file.includes('valueset') &&
-        !file.includes('codesystem') &&
-        !file.includes('structuredefinition')
-      ) {
-        const buf = fs.readFileSync(`${examplesDir}/${file}`)
+        file.endsWith('json') &&
+        file.includes('example') &&
+        doNotSkip(file, specDir)
+        ) {
+        const buf = fs.readFileSync(`${specDir}/${file}`)
         const content = buf.toString()
-        const resource = JSON.parse(content)
-        const { resourceType } = resource
-        delete resource.text
-        const resourceWoComments = removeKeys(resource, [ 'fhir_comments' ])
-        testFile.push(`dut = ${JSON.stringify(resourceWoComments)} as fhir.${resourceType};`)
+        try {
+          const resource = JSON.parse(content)
+          const { resourceType } = resource
+          delete resource.text
+          const resourceWoComments = cleanEmpty(removeKeys(resource, [ 'fhir_comments' ]))
+
+          testFile.push('')
+          testFile.push(`// ${file}`)
+          testFile.push(`const test${i}: fhir.${resourceType} = ${JSON.stringify(resourceWoComments)};`)
+        } catch(e) {
+          console.error(`error with ${file}`, e)
+        }
       }
     })
+
+    testFile.push('') // tslint wants an extra line
 
     console.log(`* Writing to ${outputDir}/fhir-test.ts...`)
     fs.writeFile(`${outputDir}/fhir-tests.ts`, testFile.join("\n"), (err) => {
@@ -82,18 +119,40 @@ export const buildExamples = (examplesDir: string, outputDir: string, cb: Functi
   })
 }
 
+export const copyTs = (version: string, cb: Function) => {
+  const src = `${__dirname}/fhir-codegen/generated/TypeScript_${version.toUpperCase()}.ts`
+  const dest = `${__dirname}/types/${version}/index.d.ts`
 
-export const createR2Examples = (cb: Function) => {
-  const examplesDir = `${__dirname}/examples`
-  const testDir = `${__dirname}/build/r2`
-  download('https://hl7.org/fhir/DSTU2/examples-json.zip', `${examplesDir}/examples-r2.zip`, () => {
-    buildExamples(`${examplesDir}/examples-r2`, testDir, cb)
+  const header = `// Type definitions for FHIR ${version.slice(1)}.0
+// Project: http://hl7.org/fhir/index.html
+// Definitions by: Artifact Health <https://github.com/meirgottlieb>
+//                 Jan Huenges <https://github.com/jhuenges>
+//                 Brian Kaney <https://github.com/bkaney>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTypes
+//
+// Generated from https://github.com/microsoft/fhir-codegen, packaged
+// with https://github.com/vermonster/fhir-dt-generator.
+//
+
+`
+
+  console.log(`* Extracting ${src} to ${dest}, adding header, and removing trailing whitespaces...`)
+  execute(`echo '${header}' | cat - ${src} > ${dest} && sed -i 's/[[:space:]]*$//' ${dest}`, cb)
+}
+
+export const createR2 = (cb: Function) => {
+  const specDir = `${__dirname}/spec`
+  const testDir = `${__dirname}/types/r2`
+  download('http://hl7.org/fhir/DSTU2/fhir-spec.zip', `${specDir}/r2.zip`, () => {
+    buildExamples(`${specDir}/r2/site`, testDir, () => {
+      copyTs('r2', cb)
+    })
   })
 }
 
 export const clean = () => del([ 'build', 'examples' ])
 export const mkdir = (cb: Function) => {
-  fs.mkdirSync('examples') 
+  fs.mkdirSync('examples')
   fs.mkdirSync('build/r2', {recursive: true})
   fs.mkdirSync('build/r3', {recursive: true})
   fs.mkdirSync('build/r4', {recursive: true})
@@ -103,4 +162,4 @@ export const mkdir = (cb: Function) => {
 
 // const build = gulp.series(clean, mkdir, parallel(download_2, download_3, download_4))
 // export default gulp.series(clean, mkdir, createR2Examples)
-export default gulp.series(createR2Examples)
+export default gulp.series(createR2)
